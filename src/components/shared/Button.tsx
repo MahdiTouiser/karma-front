3b5d9592a897781425dcaee3803a840eeb4d9fa2
Mahdi Touiserkani <mahditouserkani@yahoo.com>
2024-05-13 11:24:26 +0330

    Fix the build errors

--- src/components/shared/Button.tsx
+++ src/components/shared/Button.tsx
@@ -1,13 +1,14 @@
-import { Button, ButtonProps, DeepPartial, FlowbiteButtonTheme } from "flowbite-react";
+import { Button, ButtonProps, FlowbiteButtonTheme } from "flowbite-react";
+import { DeepPartial } from "react-hook-form";
 interface SDButtonProps extends ButtonProps {
   children?: React.ReactNode;
 }
 
 const customTheme: DeepPartial<FlowbiteButtonTheme> = {
   color: {
-    ...Button.defaultProps?.theme?.color,
+    // ...Button.defaultProps?.theme?.color,
     primary2: 'bg-primary2-500 hover:bg-primary2-600 text-white  disabled:hover:bg-primary2-500',
-    primary2: 'bg-primary2-800 hover:bg-primary2-900 text-white disabled:hover:bg-primary2-800',
+    // primary2: 'bg-primary2-800 hover:bg-primary2-900 text-white disabled:hover:bg-primary2-800',
   },
   pill: {
     off: 'rounded-sm'
