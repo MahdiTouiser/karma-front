02477176882584c554b75fef3e749abbab891b66
Mahdi Touiserkani <mahditouserkani@yahoo.com>
2024-05-14 14:20:57 +0330

    Implement the basic info section

---
+++ src/components/userPanel/Resume/EducationalHistory.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import KCard from '../../shared/Card'
+
+const EducationalHistory: React.FC = () => {
+    return (
+        <KCard>EducationalHistory</KCard>
+    )
+}
+
+export default EducationalHistory
--- src/components/userPanel/Resume/MyResume.tsx
+++ src/components/userPanel/Resume/MyResume.tsx
@@ -1,19 +1,20 @@
 import React from 'react'
 import AboutMe from './AboutMe'
 import BasicInfo from './BasicInfo'
+import EducationalHistory from './EducationalHistory'
 
 const MyResume: React.FC = () => {
     return (
         <>
-            <div className='flex flex-col mt-10 w-screen px-10'>
+            <div className='flex flex-col mt-10 w-screen pr-16'>
                 <div className='w-3/5'>
                     <AboutMe />
                 </div>
-                <div className='w-3/5 mt-10 flex justify-center items-center'>
+                <div className='w-3/5 mt-6 flex justify-center items-center'>
                     <BasicInfo />
                 </div>
-                <div>
-
+                <div className='w-3/5 mt-6 flex justify-center items-center'>
+                    <EducationalHistory />
                 </div>
             </div>
         </>
