c26856979e96d1829c97a8b254938fe996e0c367
Mahdi Touiserkani <mahditouserkani@yahoo.com>
2024-05-17 21:26:50 +0330

    Implement the Footer

---
+++ src/pages/userPanel/Home/Footer.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer: React.FC = () => {
+    return (
+        <div className='bg-blue-900 p-6 '>
+            <div className='flex justify-center'>
+                <p className='text-white'>
+                    Footer
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
