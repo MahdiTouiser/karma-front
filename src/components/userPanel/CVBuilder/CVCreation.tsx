ff3aa23a646107d5b4c5a4cf5e848254ef993876
Mahdi Touiserkani <mahditouserkani@yahoo.com>
2024-05-19 11:52:36 +0330

    Fix the styling of the components

--- src/components/userPanel/CVBuilder/CVCreation.tsx
+++ src/components/userPanel/CVBuilder/CVCreation.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import KCard from '../../shared/Card'
-import UserSideber from '../SignupSidebar'
+import SignupSidebar from '../SignupSidebar'
 
 const CVCreation: React.FC = () => {
     return (
-        <KCard withPadding={false} className='flex justify-start m-20'>
-            <UserSideber isMenuOpen={false} toggleMenu={function (): void {
-                throw new Error('Function not implemented.')
-            }} />
-        </KCard>
+        <>
+            <KCard withPadding={false} className='flex justify-start my-10'>
+                <SignupSidebar />
+            </KCard>
+        </>
     )
 }
 
--- src/components/userPanel/SignupSidebar.tsx
+++ src/components/userPanel/SignupSidebar.tsx
@@ -1,29 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { LinkWithIcon } from "../../models/shared.models";
-import { ShellElement } from "../shared/PanelShell";
 
-const SignupSidebar: React.FC<ShellElement> = () => {
+const SignupSidebar: React.FC = () => {
+  const [activeIndex, setActiveIndex] = useState<number>(0);
+
   const links: LinkWithIcon[] = [
-    {
-      title: "اطلاعات اولیه",
-    },
-    {
-      title: "سوابق تحصیلی",
-    },
-    {
-      title: "سوابق شغلی",
-    },
-    {
-      title: "مهارت های تکمیلی",
-    },
+    { title: "اطلاعات اولیه" },
+    { title: "سوابق تحصیلی" },
+    { title: "سوابق شغلی" },
+    { title: "مهارت های تکمیلی" },
   ];
 
+  const handleClick = (index: number) => {
+    setActiveIndex(index);
+  };
+
   return (
-    <div className="bg-green-500">
+    <div className="border-l-2">
       {links.map((link, index) => (
         <div
           key={index}
-          className="font-extrabold bg-gray-100 text-green-500 text-2xl border-r-2 border-green-500 py-10 px-20"
+          className={`font-extrabold text-2xl p-10 m-5 cursor-pointer transition-colors duration-300 ease-in-out 
+          ${activeIndex === index ? "text-green-500 border-r-4 border-green-500" : "text-gray-200 border-r-2 border-transparent hover:border-green-500"}`}
+          onClick={() => handleClick(index)}
         >
           <span>{link.title}</span>
         </div>
