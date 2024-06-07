49cbc2180b3dc241662aa11eb3312d767d65c27d
Mahdi Touiserkani <mahditouserkani@yahoo.com>
2024-06-07 12:30:21 +0330

    Implement the languages sections

--- src/components/user-panel/my-resume/Languages.tsx
+++ src/components/user-panel/my-resume/Languages.tsx
@@ -1,35 +1,95 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
 import Add from '../../../assets/icons/Add';
+import Delete from '../../../assets/icons/Delete';
+import useApi from '../../../hooks/useApi';
+import useConfirm from '../../../hooks/useConfirm';
+import { LanguagesData } from '../../../models/cvbuilder.models';
+import { BaseResponse } from '../../../models/shared.models';
 import KCard from '../../shared/Card';
+import KSpinner from '../../shared/Spinner';
+import LanguagesModal from '../cv-builder/sections/additional-skills/languages/LanguagesModal';
 
 const Languages: React.FC = () => {
-    const LanguageData = [
-        { language: 'انگلیسی', level: 'پیشرفته' },
-        { language: 'آلمانی', level: 'مقدماتی' },
-    ];
+    const { sendRequest: fetch, isPending } = useApi<null, LanguagesData[]>();
+    const [languages, setLanguages] = useState<LanguagesData[]>([]);
+    const { sendRequest: deleteRequest } = useApi<null, BaseResponse<null>>();
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const openModal = () => setIsModalOpen(true);
+    const closeModal = () => setIsModalOpen(false);
+    const [ConfirmModal, confirmation] = useConfirm(
+        "آیا از حذف این آیتم مطمئنید؟",
+        "حذف مهارت زبان خارجی"
+    );
+
+    const handleDeleteItem = async (id: number) => {
+        const confirm = await confirmation();
+        if (confirm) {
+            deleteRequest(
+                {
+                    url: `/Resumes/RemoveLanguage/${id}`,
+                    method: 'delete'
+                },
+                (response) => {
+                    toast.success(response?.message);
+                    fetchLanguages()
+                },
+                (error) => {
+                    toast.error(error?.message);
+                }
+            );
+        }
+    }
+
+    const fetchLanguages =
+        () => {
+            fetch(
+                {
+                    url: '/Resumes/Languages',
+                },
+                (response) => {
+                    setLanguages(response);
+                },
+            );
+        };
+
+    useEffect(() => {
+        fetchLanguages();
+    }, []);
 
     return (
-        <KCard className='flex flex-col justify-between w-full'>
-            <div className="flex items-center justify-between">
-                <h1 className='text-xl font-extrabold'>زبان ها</h1>
-                <button className="text-sm text-blue-500 flex items-center">
-                    <Add />
-                    افزودن
-                </button>
-            </div>
-            <div className="grid grid-cols-4 gap-2 mt-5">
-                {LanguageData.map((info, index) => (
-                    <div key={index} className="flex items-center px-2 py-1 bg-gray-200 rounded">
-                        <span className='ml-4'>
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-                            </svg>
-                        </span>
-                        <p className='text-black text-sm'>{info.language} | {info.level}</p>
+        <>
+            <ConfirmModal />
+            <LanguagesModal show={isModalOpen} onClose={closeModal} onSuccess={fetchLanguages} />
+
+            <KCard className='flex flex-col justify-between w-full'>
+                <div className="flex items-center justify-between">
+                    <h1 className='text-xl font-extrabold'>زبان ها</h1>
+                    <button className="text-sm text-blue-500 flex items-center" onClick={openModal}>
+                        <Add />
+                        افزودن
+                    </button>
+                </div>
+                {isPending ? (
+                    <div className='flex justify-center'>
+                        <KSpinner color='primary' size={10} />
                     </div>
-                ))}
-            </div>
-        </KCard>
+                ) : (
+                    <div className="grid grid-cols-4 gap-2 mt-5">
+                        {languages.map((info, index) => (
+                            <div key={index} className="flex items-center px-2 py-1 bg-gray-200 rounded">
+                                <button onClick={() => handleDeleteItem(info.id)} className="ml-4">
+                                    <Delete />
+                                </button>
+                                <p className='text-black text-sm'>{info.Language.title} | {info.languageLevel}</p>
+                            </div>
+                        ))}
+                    </div>
+                )}
+            </KCard>
+
+        </>
+
     );
 }
 
