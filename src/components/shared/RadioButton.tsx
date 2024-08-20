66ee012241d7973642c938d3a53970f381b82347
Mahdi Touiserkani <mahditouserkani@yahoo.com>
2024-08-20 10:11:58 +0330

    Fix the auth bugs

--- src/components/shared/RadioButton.tsx
+++ src/components/shared/RadioButton.tsx
@@ -31,9 +31,8 @@ const KRadioButton = forwardRef<HTMLInputElement, RadioButtonProps>(
     const { onChange, ...restRegister } = register;
 
     return (
-      <div className="flow-root border-gray-300 bg-white border rounded-md">
+      <div className="flow-root bg-white border border-gray-300 rounded-md">
 
-        {/* Render select box for lg devices and smaller */}
         <div className="lg:hidden">
           <select
             className="block w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-cyan-500"
@@ -49,7 +48,7 @@ const KRadioButton = forwardRef<HTMLInputElement, RadioButtonProps>(
           </select>
         </div>
 
-        <div className="hidden lg:flex lg:flex-row w-full">
+        <div className="hidden w-full lg:flex lg:flex-row">
           {options.map((option) => (
             <label
               key={option.value}
--- src/pages/Auth/AuthContainer.tsx
+++ src/pages/Auth/AuthContainer.tsx
@@ -4,6 +4,7 @@ import {
 } from 'react';
 
 import karmaLogo from '/src/assets/karma-logo.png';
+import backgroundPhoto from '/src/assets/protection-concept-with-pawns.jpg';
 import {
   Navigate,
   Outlet,
@@ -21,37 +22,44 @@ const AuthContainer = () => {
     setWasAuthenticated(!!authData);
   }, []);
 
-  const shouldRedirect = (authState.isAuthenticated &&
-    authState.personalInformationCompleted &&
-    authState.securityInformationCompleted) || wasAuthenticated;
+  const shouldRedirect =
+    (authState.isAuthenticated &&
+      authState.personalInformationCompleted &&
+      authState.securityInformationCompleted) ||
+    wasAuthenticated;
 
-  return (
-    shouldRedirect && !authState.enteredPhone ? (
-      <Navigate to="/" />
-    ) : (
-      <div className="bg-cyan-100 h-screen flex justify-center items-center lg:flex-row">
-        <div className="hidden lg:flex flex-1 justify-center items-center h-screen">
-          <div className="text-center">
-            <h1 className="text-4xl font-bold">کارما</h1>
-            <p className="mt-2 text-xl">
-              استخدام و ساختن مسیر شغلی مطلوب برای کارجو
-            </p>
-          </div>
-        </div>
-
-        <div className="w-full flex flex-col items-center max-w-lg shadow-lg bg-white m-0 lg:mx-32 justify-center h-full lg:h-screen">
-          <div className="pt-5 border-b w-11/12 flex justify-center flex-col items-center">
-            <span className="w-48 h-48 flex justify-center items-center">
-              <img src={karmaLogo} alt="Karma Logo" />
-            </span>
-          </div>
-          <div className="flex flex-col items-center justify-center w-full mt-5">
-            <Outlet />
-          </div>
+  return shouldRedirect && !authState.enteredPhone ? (
+    <Navigate to="/" />
+  ) : (
+    <div className="flex items-center justify-center min-h-screen bg-cyan-100 lg:flex-row">
+      <div
+        className="items-center justify-center flex-1 hidden lg:flex"
+        style={{
+          backgroundImage: `url(${backgroundPhoto})`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
+          minHeight: '100vh',
+        }}
+      >
+        <div className="p-8 text-center bg-black bg-opacity-50 rounded">
+          <h1 className="text-4xl font-bold text-white">کارما</h1>
+          <p className="mt-2 text-xl text-gray-200">
+            استخدام و ساختن مسیر شغلی مطلوب برای کارجو
+          </p>
         </div>
       </div>
-    )
+
+      <div className="flex flex-col items-center justify-center w-full h-full max-w-lg p-8 bg-white shadow-lg lg:h-screen">
+        <div className="flex flex-col items-center justify-center w-full pb-5 border-b">
+          <img src={karmaLogo} alt="Karma Logo" className="w-48 h-48" />
+        </div>
+        <div className="flex flex-col items-center justify-center w-full mt-5">
+          <Outlet />
+        </div>
+      </div>
+    </div>
   );
-}
+};
 
 export default AuthContainer;
